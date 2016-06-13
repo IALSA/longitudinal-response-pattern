70fee295ef725e1261ef6305a5da8941d260864d
Andrey Koval <koval.andrey@gmail.com>
2016-06-13 13:59:52 -0700

    #1 response pattern is printed

--- sandbox/2016-06-13-basic-example/missing-patterns.R
+++ sandbox/2016-06-13-basic-example/missing-patterns.R
@@ -64,6 +64,8 @@ varnames <- names(ds4w %>%
                     dplyr::select(contains("stroke_status")))
 dots = c("projid", varnames)
 ds <- ds4w %>% dplyr::select_(.dots = dots)
+# ds <- ds[,ds==NA]
+ds[is.na(ds)] <- "."
 # ds <- ds[,1:4]
 head(ds)
 
@@ -74,15 +76,25 @@ varnames
 
 # Create a string variables that records the response pattern of each individual
 # see http://stackoverflow.com/questions/14568662/paste-multiple-columns-together-in-r
-
-ds$pattern <- apply( ds[, varnames], 1 , paste , collapse = "-" )
+ds$pattern <- apply( ds[, varnames], 1 , paste , collapse = "" )
 # remove the unnecessary rows
 keep_variables <- setdiff(colnames(ds), varnames)
 ds <- as.data.frame(ds[ , keep_variables ])
-        
+
+ds_pattern <- ds %>% 
+  dplyr::group_by(pattern) %>% 
+  dplyr::summarize(count = n()) %>% 
+  dplyr::arrange(desc(count))
+ds_pattern
 
 
-
+ds_pattern %>%
+  DT::datatable(
+    class     = 'cell-border stripe',
+    caption   = "Pattern of missingness across time points",
+    filter    = "top",
+    options   = list(pageLength = 6, autoWidth = TRUE)
+  )
 
   
 
