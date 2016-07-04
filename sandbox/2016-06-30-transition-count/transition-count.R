96e8513a383bf59d04cf3db768620135a903fcc1
knighttime <emajjunk@gmail.com>
2016-07-04 15:34:24 -0700

    2016-07-04 updated files

--- sandbox/2016-06-30-transition-count/transition-count.R
+++ sandbox/2016-06-30-transition-count/transition-count.R
@@ -22,16 +22,16 @@ requireNamespace("testit")# For asserting conditions meet expected patterns.
 if (!require("DT")) install.packages('DT')
 
 # ---- declare-globals ---------------------------------------------------------
-dataPath <- "./data/unshared/derived/ds0_raw.rds"
+dataPath <- "../MAP/data-unshared/raw/ds0_raw.rds"
 
 # ---- load-data ---------------------------------------------------------------
 ds0 <- readRDS(dataPath)
 # ---- inspect-data -------------------------------------------------------------
-variables_to_select <- c("projid", "fu_year",  "age_death", "age_at_visit", "age_bl","stroke_cum")
+variables_to_select <- c("id", "fu_year",  "age_death", "age_at_visit", "age_bl","stroke_cum")
 # variables_to_select <- c("projid", "fu_year", "stroke_cum")
 ds_long <- ds0 %>% 
-  dplyr::select_(.dots = variables_to_select) %>% 
-  dplyr::rename(id = projid)
+  dplyr::select_(.dots = variables_to_select) # %>%
+  # dplyr::rename(id = projid)
 # recode NA 
 ds_long$stroke_cum[is.na(ds_long$stroke_cum)] <- "."
 # ds <- ds[,1:4]
@@ -47,13 +47,24 @@ ds_long %>%
 
 # ---- tweak-data --------------------------------------------------------------
 
+#check for duplicates
+dsd0 <- ds_long %>% dplyr::group_by(id, fu_year) %>% dplyr::filter(n()>1) %>% dplyr::summarise(n=n())
+# dsd0 shoud have no duplicates. Obs should equal 0
+# July 4, 2016: there are many duplicates - inviestigate why this is in MAP curator
+rm(dsd0)
+
+#remove duplicates for now
+ds_distinct <- ds_long %>%
+  dplyr::distinct()
+
+
 # ----- long-to-wide ------
 # define variable properties for long-to-wide conversion
 variables_static <- c("id", "age_death", "age_bl")
-variables_longitudinal <- setdiff(colnames(ds_long),variables_static)  # not static
+variables_longitudinal <- setdiff(colnames(ds_distinct),variables_static)  # not static
 (variables_longitudinal <- variables_longitudinal[!variables_longitudinal=="fu_year"]) # all except wave
 # establish a wide format
-ds_wide <- ds_long %>%
+ds_wide <- ds_distinct %>%
   dplyr::filter(!is.na(fu_year)) %>% 
   # dplyr::select(id, wave, animals, word_recall_de ) %>%
   # gather(variable, value, -(id:wave)) %>%
@@ -64,44 +75,35 @@ ds_wide <- ds_long %>%
   tidyr::spread(temp, value)
 ds_wide %>% dplyr::glimpse()
 
+names(ds_wide)
+
 knitr::kable(ds_wide %>% 
-  dplyr::group_by(temp) %>% 
-  dplyr::summarize(n()))
+               dplyr::group_by(temp) %>% 
+               dplyr::summarize(n()))
 
 
-
-# take the final data set and get a wide version of it and an na.omit subset
-
-#reshape data from long to wide
-names(d)
-
-max(ds$fu_year, na.rm = TRUE) #18
-
-#time invariant : -1, -2, -4, -6, -7, -9:-16
-time_variant <- c("age_at_visit", "stroke_cum")
-measures <- time_variant
-measures <- "stroke_cum"
-(measures <- names(d)[c(-1,-2)])
-# measures <- names(ds)[c(-1,-2, -4, -6, -7, -9:-16)] #discluded projid and fu_year
-measures
-
-dsw <- stats::reshape(d, v.names=measures, timevar='fu_year', idvar='projid', direction="wide")
-names(dsw)
-
-ds_wide <- ds_long %>%
-  tidyr::spread(key=variable,value=value) %>%
-  dplyr::arrange_(.dots=time_invariant)
 
 # ---- missing-values-table --------------------------------------------------------------
 
 
 # ---- binary-values-table --------------------------------------------------------------
+# using ds_wide data
 
+# select the variables over which you'd like to view response pattern
+varnames <- names(ds_wide %>%
+                    dplyr::select(contains("stroke_cum")))
+dots = c("id", varnames)
+d <- ds_wide %>% dplyr::select_(.dots = dots)
+# recode NA 
+d[is.na(d)] <- "."
+# ds <- ds[,1:4]
+# head(d)
 
 # Create a string variables that records the response pattern of each individual
 # see http://stackoverflow.com/questions/14568662/paste-multiple-columns-together-in-r
-varnames <- "stroke_cum"
-d$pattern <- apply( d[,varnames], 1 , paste , collapse = "" )
+# varnames <- "stroke_cum"
+d$pattern <- apply(d[,varnames], 1 , paste , collapse = "" )
+
 # remove the unnecessary rows
 keep_variables <- setdiff(colnames(d), varnames)
 d <- as.data.frame(d[ , keep_variables ])
@@ -126,3 +128,6 @@ rmarkdown::render(input = "./sandbox/2016-06-17-pattern-table/response-pattern-f
 
 
 # @knitr Table --------------------
+
+# ---- count-transitions ---------------------------------------
+# count the transitions from no stroke to stroke
