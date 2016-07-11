6a6ea5dbe39c01f1921cd3a654eb70e3e37a5311
Andrey Koval <koval.andrey@gmail.com>
2016-07-11 16:05:16 -0700

    #6 stabilized subsetting

--- manipulation/7-subsets-for-analysis.R
+++ manipulation/7-subsets-for-analysis.R
@@ -38,8 +38,8 @@ ds <- ds %>%
     id = as.integer(projid)
   )
 
-# ---- subset-variables ---------------------------------
-t <- table(ds$fu_year, ds$cdx); t[t==0] <- "."; t
+# ---- subset-variables-cdx-1 ---------------------------------
+t <- table(ds$fu_year, ds$cdx, useNA = "always"); t[t==0] <- "."; t
 names(ds)
 
 keep_these_ids <-  ds %>% 
@@ -49,60 +49,116 @@ keep_these_ids <-  ds %>%
 keep_these_ids <- as.integer(keep_these_ids$id)
 length(unique(keep_these_ids)) # 1236
 length(unique(ds$id)) # 1803
-
-ds_subset <- ds %>% 
+ 
+ds_subset_1 <- ds %>% 
   dplyr::filter(id %in% keep_these_ids)
 
-length(unique(ds_subset$id))
+length(unique(ds_subset_1$id))
 
-ds_subset %>% 
+ds_subset_1 %>% 
   # dplyr::slice(1:10)
   dplyr::group_by(cdx) %>% 
   dplyr::summarize(count = n())
 
-t <- table(ds_subset$fu_year, ds_subset$cdx); t[t==0] <- "."; t
+t <- table(ds_subset_1$fu_year, ds_subset_1$cdx, useNA = "always"); t[t==0] <- "."; t
+names(ds)
+
+# ---- subset-variables-smell-test-2 ---------------------------------
+t <- table(ds$fu_year, ds$total_smell_test, useNA = "always"); t[t==0] <- "."; t
+t <- table(ds_subset_1$fu_year, ds_subset_1$total_smell_test, useNA = "always"); t[t==0] <- "."; t
+names(ds)
+
+keep_these_ids <- ds_subset_1 %>% 
+  dplyr::select(id, fu_year, total_smell_test) %>% 
+  dplyr::filter(fu_year == 0, !is.na(total_smell_test)) %>% 
+  dplyr::select(id)
+keep_these_ids <- as.integer(keep_these_ids$id)
+length(unique(keep_these_ids)) # 1054
+length(unique(ds$id)) # 1803
+
+ds_subset_2 <- ds_subset_1 %>% 
+  dplyr::filter(id %in% keep_these_ids)
+
+length(unique(ds_subset_2$id))
+
+ds_subset_2 %>% 
+  # dplyr::slice(1:10)
+  dplyr::group_by(total_smell_test) %>% 
+  dplyr::summarize(count = n())
+
+t <- table(ds_subset_2$fu_year, ds_subset_2$total_smell_test, useNA = "always"); t[t==0] <- "."; t
+names(ds)
+
+
+# ---- subset-variables-smell-test-3 ---------------------------------
+t <- table(ds_subset_2$fu_year, ds_subset_2$total_smell_test, useNA = "always"); t[t==0] <- "."; t
+
+
+keep_these_ids <- ds_subset_2 %>% 
+  dplyr::select(id, fu_year, total_smell_test) %>% 
+  dplyr::group_by(id) %>% 
+  dplyr::summarize(tst_count = n()) %>% 
+  dplyr::filter(!tst_count %in% c(1,2)) %>%
+  dplyr::select(id)
+keep_these_ids <- as.integer(keep_these_ids$id)
+length(unique(keep_these_ids)) # 890
+length(unique(ds$id)) # 1803
+
+ds_subset_3 <- ds_subset_2 %>% 
+  dplyr::filter(id %in% keep_these_ids) %>% 
+  dplyr::group_by(id) %>% 
+  dplyr::mutate(tst_count = n()) %>% 
+  dplyr::ungroup()
+
+
+ds_subset_3 %>% 
+  # dplyr::slice(1:10)
+  dplyr::group_by(tst_count) %>% 
+  dplyr::summarize(count = n()) 
+
+t <- table(ds_subset_3$fu_year, ds_subset_3$tst_count, useNA = "always"); t[t==0] <- "."; t
 names(ds)
 
 
 # ---- subset-base -----------------
-
-# base subset should only include people who do not have dementia at baseline
-#change original file ie. ds to be each of the subsets above.
-
-sub1<-ds
-
-#subset to remove any participants with dementia at baseline
-#cdx is assesment while alive, 1:normal, 2-3:mild MCI, 4-5:AD
-varsREM <- sub1[ which(sub1$fu_year==0 & sub1$cdx != 1),]
-
-
-sub2 <- sub1
-for (rows in varsREM["projid"]) {
-  for (id in rows) {
-    sub2 <- sub2[sub2$projid != id, ]
-  }
-}
-
-dplyr::n_distinct(sub2$projid)#ds: 1239 (564 less)|ds1:392 | ds2:1239
-dplyr::n_distinct(sub1$projid) #ds:1803 subjects | ds1:650 | ds2:1660
-dplyr::n_distinct(varsREM$projid) #ds: #564 | ds1:258 | ds2: 421
-#correct numer was removed
-
-#sub2 = all people who were cognitively normal at baseline 
-
-# rename the subsets
-dsbase <- sub2 # 1239
-dplyr::n_distinct(dsbase$projid)
-max(dsbase$fu_year)# NA, why not working?
-dsbase$projid %>% unique() %>% length() 
-                  
-ds1base <- sub2 # 392, people who have BSIT and niareagansc
-max(ds1base$fu_year) #17
-dplyr::n_distinct(ds1base$projid)
-
-ds2base<- sub2 #1239, people who have BSIT
-max(ds2base$fu_year) #18
-dplyr::n_distinct(ds2base$projid)
+# 
+# # base subset should only include people who do not have dementia at baseline
+# #change original file ie. ds to be each of the subsets above.
+# 
+# sub1<-ds
+# 
+# #subset to remove any participants with dementia at baseline
+# #cdx is assesment while alive, 1:normal, 2-3:mild MCI, 4-5:AD
+# varsREM <- sub1[ which(sub1$fu_year==0 & sub1$cdx != 1),]
+# 
+# 
+# sub2 <- sub1
+# for (rows in varsREM["projid"]) {
+#   for (id in rows) {
+#     sub2 <- sub2[sub2$projid != id, ]
+#   }
+# }
+# 
+# dplyr::n_distinct(sub2$projid)#ds: 1239 (564 less)|ds1:392 | ds2:1239
+# dplyr::n_distinct(sub1$projid) #ds:1803 subjects | ds1:650 | ds2:1660
+# dplyr::n_distinct(varsREM$projid) #ds: #564 | ds1:258 | ds2: 421
+# #correct numer was removed
+# 
+# #sub2 = all people who were cognitively normal at baseline 
+# 
+# # rename the subsets
+# dsbase <- sub2 # 1239
+# dplyr::n_distinct(dsbase$projid)
+# max(dsbase$fu_year)# NA, why not working?
+# dsbase$projid %>% unique() %>% length() 
+#                   
+# ds1base <- sub2 # 392, people who have BSIT and niareagansc
+# max(ds1base$fu_year) #17
+# dplyr::n_distinct(ds1base$projid)
+# 
+# ds2base<- sub2 #1239, people who have BSIT
+# max(ds2base$fu_year) #18
+# dplyr::n_distinct(ds2base$projid)
 
 #ds and ds2 are the same number of people?
 #use ds1 for autopsy pathology investigation and ds2 as other
